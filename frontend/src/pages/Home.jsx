540925f221e76eb402236b356a16c250557fd84d
Ajene <ajenec@gmail.com>
2025-05-12 10:39:01 -0400

    start over set up done AJ

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,20 @@
 export default function HomePage() {
-  return <>
-    <h1>Home</h1>
-    <p>Put something interesting here!</p>
-  </>
+  return (
+    <>
+      <div>
+        <h1>Get Inspired Today!</h1>
+        <p>Join a platform for challenges and goals. Share with the world.</p>
+        <button>Join Here</button>
+      </div>
+      <div>
+        <p>
+          Mindful Motion is a platform to maximize teenagers’ productivity and
+          ultimately boost their ambition by creating a web app that allows for
+          users to continue to get their dopamine rush obtained from scrolling
+          on an app, but control the content being fed to them by only hosting
+          projects and challenges to our users.
+        </p>
+      </div>
+    </>
+  );
 }
