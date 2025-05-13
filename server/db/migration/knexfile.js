917e7015b4e31e73a998a5c7a4d2ded05f878f9e
Tyron <tyronodame5@gmail.com>
2025-05-13 14:52:47 -0400

    fixed bugs with seeds

---
+++ server/db/migration/knexfile.js
@@ -0,0 +1,59 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+require('dotenv').config();
+const path = require('path');
+const migrationsDirectory = path.join(__dirname, 'db/migration/migrations');
+const seedsDirectory = path.join(__dirname, '/db/seeds');
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      user: 'postgres', // unless you want to use a different user
+      password: 'postgres', // unless you changed your password
+      database: 'mindfulv2',
+    },
+    migrations: {
+      directory: migrationsDirectory,
+      tableName: 'knex_migrations',
+    },
+    seeds: {
+      directory: seedsDirectory,
+    },
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+};
