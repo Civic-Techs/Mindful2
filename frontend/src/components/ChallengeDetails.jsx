901a4aa4bc7cb63923c29f636ef98951a23171ed
Tyron <tyronodame5@gmail.com>
2025-05-14 13:45:54 -0400

    configured the join button to populate table

--- frontend/src/components/ChallengeDetails.jsx
+++ frontend/src/components/ChallengeDetails.jsx
@@ -1,10 +1,20 @@
-import { getChallengeId } from "../adapters/challengesFetch";
-import { Link, useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { getChallengeId } from '../adapters/challengesFetch';
+import { Link, useParams, useNavigate } from 'react-router-dom';
+import { useEffect, useState, useContext } from 'react';
+import CurrentUserContext from '../contexts/current-user-context';
+import { fetchHandler, getPostOptions } from '../utils/fetchingUtils';
 
 function ChallengeInfo() {
   const { id } = useParams();
   const [challenge, setChallenge] = useState(null);
+
+  const { currentUser } = useContext(CurrentUserContext);
+  console.log({ currentUser });
+  const navigate = useNavigate();
+  if (!currentUser) {
+    console.log('No current user, redirecting to login');
+    navigate('/login');
+  }
 
   useEffect(() => {
     const getChallengeInfo = async () => {
@@ -12,21 +22,39 @@ function ChallengeInfo() {
         const [data, error] = await getChallengeId(id);
 
         // CHECKING DATA
-        console.log("challenge:", data);
+        console.log('challenge:', data);
         //CHECKING DATA
 
         if (error) {
-          console.error("Error fetching challenge:", error);
+          console.error('Error fetching challenge:', error);
           return;
         }
 
         setChallenge(data);
       } catch (error) {
-        console.error("Error fetching challenge:", error);
+        console.error('Error fetching challenge:', error);
       }
     };
     getChallengeInfo();
   }, [id]);
+
+  const handleJoin = async () => {
+    const currentUserId = currentUser.id;
+    console.log(currentUserId);
+    // get info on the challenge
+    try {
+      // const [challengeData, error] = await getChallengeId(id);
+      return await fetchHandler(
+        `/api/participants`,
+        getPostOptions({ id, currentUserId })
+      );
+
+      if (error) {
+        console.error('Error fetching challenge:', error);
+        return;
+      }
+    } catch (error) {}
+  };
 
   if (!challenge) return <p>Loading...</p>;
 
@@ -34,13 +62,13 @@ function ChallengeInfo() {
     <>
       <h2>{challenge.title}</h2>
       <p>Description: {challenge.description}</p>
-      <p>Contest: {challenge.contest ? "yes" : "no"}</p>
-      <p>Winner:{challenge.winner ? "yes" : "no"}</p>
+      <p>Contest: {challenge.contest ? 'yes' : 'no'}</p>
+      <p>Winner:{challenge.winner ? 'yes' : 'no'}</p>
       <p>Start Date: {challenge.created_at}</p>
       <p>End: {challenge.end_time}</p>
-      <button>Join</button>
+      <button onClick={handleJoin}>Join</button>
 
-      <Link to={"/challenges"}>
+      <Link to={'/challenges'}>
         <button>Back to Challenges</button>
       </Link>
     </>
--- frontend/src/pages/Challenges.jsx
+++ frontend/src/pages/Challenges.jsx
@@ -1,6 +1,6 @@
-import { allChallenges } from "../adapters/challengesFetch";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { allChallenges } from '../adapters/challengesFetch';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function ChallengesPage() {
   const [allChallengesData, setAllChallengesData] = useState([]);
@@ -10,10 +10,10 @@ function ChallengesPage() {
       const [data, error] = await allChallenges();
 
       // CHECKING DATA
-      console.log("challenge data:", data);
+      console.log('challenge data:', data);
       //CHECKING DATA
       if (error) {
-        console.error("Error fetching challenges:", error);
+        console.error('Error fetching challenges:', error);
         return;
       }
       setAllChallengesData(data);
@@ -23,7 +23,7 @@ function ChallengesPage() {
   return (
     <>
       <h2>Choose a Challenge and Get Started!</h2>
-      <ul style={{ listStyleType: "none", paddingLeft: 0 }}>
+      <ul style={{ listStyleType: 'none', paddingLeft: 0 }}>
         {allChallengesData.map((challenge) => (
           <li key={challenge.id}>
             <Link to={`/challenges/${challenge.id}`}>{challenge.title}</Link>
