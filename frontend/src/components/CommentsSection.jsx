4085735bc2ad82a24102e0168bf269790656dc0b
Tyron <tyronodame5@gmail.com>
2025-05-22 21:15:38 -0400

    css done

--- frontend/src/components/CommentsSection.jsx
+++ frontend/src/components/CommentsSection.jsx
@@ -1,28 +1,28 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext } from 'react';
 import {
   addComment,
   getCommentsByPostId,
   deleteComment,
-} from "../adapters/commentsFetch";
-import CurrentUserContext from "../contexts/current-user-context";
-import { getUserById } from "../adapters/user-adapter"; // Import the function to get user by ID
+} from '../adapters/commentsFetch';
+import CurrentUserContext from '../contexts/current-user-context';
+import { getUserById } from '../adapters/user-adapter'; // Import the function to get user by ID
 // import { useParams } from "react-router-dom";
 
 const CommentsSection = ({ postId }) => {
   const { currentUser } = useContext(CurrentUserContext);
   const [comments, setComments] = useState([]);
-  const [newComment, setNewComment] = useState("");
+  const [newComment, setNewComment] = useState('');
 
   useEffect(() => {
     const fetchComments = async () => {
       try {
         const [data, error] = await getCommentsByPostId(postId);
         if (error) {
-          console.error("Error fetching comments:", error);
+          console.error('Error fetching comments:', error);
           return;
         }
 
-        console.log("Fetched comments:", data.comments);
+        console.log('Fetched comments:', data.comments);
 
         // Fetch user details for each comment
         const commentsWithUserDetails = await Promise.all(
@@ -33,7 +33,7 @@ const CommentsSection = ({ postId }) => {
                 `Error fetching user for comment ${comment.id}:`,
                 userError
               );
-              return { ...comment, user: { username: "Unknown" } }; // fallback
+              return { ...comment, user: { username: 'Unknown' } }; // fallback
             }
             return { ...comment, user };
           })
@@ -41,7 +41,7 @@ const CommentsSection = ({ postId }) => {
 
         setComments(commentsWithUserDetails);
       } catch (error) {
-        console.error("Error fetching comments:", error);
+        console.error('Error fetching comments:', error);
       }
     };
 
@@ -50,7 +50,7 @@ const CommentsSection = ({ postId }) => {
 
   const handleCommentSubmit = async () => {
     if (!newComment.trim()) {
-      alert("Comment cannot be empty!");
+      alert('Comment cannot be empty!');
       return;
     }
 
@@ -61,22 +61,22 @@ const CommentsSection = ({ postId }) => {
         content: newComment,
       });
       if (error) {
-        console.error("Error submitting comment:", error);
+        console.error('Error submitting comment:', error);
         return;
       }
 
       // Fetch user details for the new comment
       const [user, userError] = await getUserById(currentUser.id);
       if (userError) {
-        console.error("Error fetching user for new comment:", userError);
+        console.error('Error fetching user for new comment:', userError);
         return;
       }
 
       setComments((prevComments) => [...prevComments, { ...comment, user }]);
 
-      setNewComment("");
+      setNewComment('');
     } catch (error) {
-      console.error("Error submitting comment:", error);
+      console.error('Error submitting comment:', error);
     }
   };
 
@@ -84,10 +84,10 @@ const CommentsSection = ({ postId }) => {
     try {
       const [response, error] = await deleteComment(id);
 
-      console.log("Delete response:", response, id);
+      console.log('Delete response:', response, id);
 
       if (error) {
-        console.error("Error deleting comment:", error);
+        console.error('Error deleting comment:', error);
         return;
       }
 
@@ -96,7 +96,7 @@ const CommentsSection = ({ postId }) => {
         prevComments.filter((comment) => comment.id !== id)
       );
     } catch (error) {
-      console.error("Error deleting comment:", error);
+      console.error('Error deleting comment:', error);
     }
   };
 
@@ -110,11 +110,12 @@ const CommentsSection = ({ postId }) => {
               <strong>{`${comment.user?.username}`}</strong> {comment.content}
               {currentUser?.id === comment.user_id && (
                 <button
+                  className="deleteButton"
                   onClick={() => {
-                    console.log("Clicked comment:", comment);
+                    console.log('Clicked comment:', comment);
                     handleDeleteComment(comment.id);
                   }}
-                  style={{ marginLeft: "10px", color: "red" }}
+                  style={{ marginLeft: '10px' }}
                 >
                   Delete
                 </button>
@@ -126,6 +127,7 @@ const CommentsSection = ({ postId }) => {
         )}
       </ul>
       <textarea
+        className="commentBox"
         value={newComment}
         onChange={(e) => setNewComment(e.target.value)}
         placeholder="Write a comment..."
